055feeb134705578c6edad1b3ee3174ccbfef0e5
nabe189 <eisuke189@gmail.com>
2024-05-10 17:20:46 +0900

    あらゆるspecに対応

--- App.py
+++ App.py
@@ -94,10 +94,12 @@ def main():
             all_specs = []
             for room in soup.find_all(class_='studio_item'):
                 room_name = room.find(class_='studio_title').text.replace(' ', '')
-                specs = [i.text.replace('広さ ','').replace('天井高 ','').replace('鏡 ', '') for i in room.find(class_='studio_spec').find_all('span')]
+                specs = [i.text.split() for i in room.find(class_='studio_spec').find_all('span')]
+                spec_name = [i[0] for i in specs]
+                specs = [i[1] for i in specs]
                 room_names.append(room_name)
                 all_specs.append(specs)
-            spec_table = pd.DataFrame(all_specs, index=room_names, columns=['広さ', '天井高', '鏡']).T
+            spec_table = pd.DataFrame(all_specs, index=room_names, columns=spec_name).T
             st.write(spec_table)
                     
 
