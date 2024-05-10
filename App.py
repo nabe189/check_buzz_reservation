282c49de7dbd6d0463d9572255ba772971c299f3
nabe189 <eisuke189@gmail.com>
2024-05-10 17:44:19 +0900

    サイト名修正

--- App.py
+++ App.py
@@ -70,9 +70,9 @@ def get_reservation_state(table):
     return marked_table
 
 def main():
-    st.title("buzz予約表一覧")
+    st.title("BUZZ予約表一覧")
 
-    st.write("東京のbuzzダンススタジオの予約表一覧です。日付を入力するとのその日の空き状況が確認できます。5人ほどなら15～20㎡、10人なら25～30㎡、それ以上なら40㎡以上が広さの目安となります。")
+    st.write("東京のBUZZスタジオの予約表一覧です。日付を入力するとのその日の空き状況が確認できます。5人ほどなら15～20㎡、10人なら25～30㎡、それ以上なら40㎡以上が広さの目安となります。")
 
     selected_date = st.date_input("日付を選択してください")
 
